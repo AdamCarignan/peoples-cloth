bd5675bfd0d41ceea7447734b7bf12b920ac32e6
Adam Carignan <acarignan100@gmail.com>
2024-07-25 13:15:18 -0400

    feat: add firebase, db, and users

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -4,10 +4,11 @@ const CategoryItem = ({category}) => {
     const { title, imageUrl } = category;
     return (
         <div className="category-container">
+            <div className="background-image" style={{
+                backgroundImage: `url(${imageUrl})`
+            }}/>
             <div className="category-body-container">
-                <div className="background-image" style={{
-                    backgroundImage: `url(${imageUrl})`
-                }}/>
+                
                 <h2>
                     {title}
                 </h2>
