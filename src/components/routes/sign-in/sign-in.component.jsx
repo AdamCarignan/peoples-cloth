9b5a7ab75521d8c815f43582104ff5e6dd9be04e
Adam Carignan <acarignan100@gmail.com>
2024-07-26 09:28:58 -0400

    feat: add button, form input, and email login

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,29 @@
-import { signInWithGoogle, creatUserDocumentFromAuth } from "../../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import {
+    signInWithGoogle,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth,
+    auth
+} from "../../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
+
     const logGoogleUser = async () => {
-        const {user} = await signInWithGoogle();
-        const userDocRef = await creatUserDocumentFromAuth(user);
+        const { user } = await signInWithGoogle();
+        const userDocRef = await createUserDocumentFromAuth(user);
         console.log(user);
     };
     return (
         <div>
             <h1>Sign In</h1>
-            <button onClick={logGoogleUser}>Sign in with Google</button>
+            <button onClick={logGoogleUser}>Sign in with Google Popo-up</button>
+            <SignUpForm />
         </div>
+
     )
 }
 export default SignIn;
